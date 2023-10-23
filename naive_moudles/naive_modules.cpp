3d22022e3f763c6ab054ebeb740e6cb7c8f12381
TaroPie1214 <53354396+TaroPie1214@users.noreply.github.com>
2023-10-23 17:34:55 +0800

    测

---
+++ naive_moudles/naive_modules.cpp
@@ -0,0 +1,13 @@
+#ifdef NAIVE_MODULES_H_INCLUDED
+ /* When you add this cpp file to your project, you mustn't include it in a file where you've
+    already included any other headers - just put it inside a file on its own, possibly with your config
+    flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+    header files that the compiler may be using.
+ */
+ #error "Incorrect use of JUCE cpp file"
+#endif
+
+#include "naive_modules.h"
+
+/** Naive DSP CPP Files*/
+#include "naive_dsp/PeakFilter.cpp"
---
+++ naive_moudles/naive_modules.h
@@ -0,0 +1,33 @@
+/** BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:            naive_moudles
+    vendor:         Naive-DSP
+    version:        0.0.1
+    name:           
+    description:    
+    website:        
+    license:        Proprietary
+
+    dependencies:   
+
+    END_JUCE_MODULE_DECLARATION
+*/
+
+#pragma once
+#define NAIVE_MODULES_H_INCLUDED
+
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_dsp/juce_dsp.h>
+#include <juce_events/juce_events.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+
+/** Naive DSP Headers*/
+#include "naive_dsp/PeakFilter.h"
